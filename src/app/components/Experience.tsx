dd6c8e0e05464a6a04fa1e66377e05a93c78a288
Abdallah Abdallaoui-maane <abdallah42.2001@icloud.com>
2024-01-12 17:10:01 +0100

    xp fini

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { BsArrowRightShort } from "react-icons/bs";
 
 interface Props {
   children: React.ReactNode;
@@ -16,7 +17,7 @@ const Experience = ({ children }: Props) => {
     setIsHovered(false);
   };
 
-  const [isClicked, setIsClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState(true);
 
   const handleClick = () => {
     setIsClicked(!isClicked);
@@ -30,12 +31,12 @@ const Experience = ({ children }: Props) => {
           <p className="text-greenlight text-2xl">02.</p>
           <p className="text-gray font-semibold text-3xl">Expérience </p>
         </div>
-        <div className="w-2/5 border-t-[0.5px] border-y-graydark"></div>
+        <div className="w-10/5 border-t-[0.5px] border-y-graydark"></div>
       </div>
-      <div className="pt-10 flex justify-between w-2/5">
+      <div className="pt-10 flex gap-10 w-10/5">
         <div
           className="relative"
-          onClick={handleClick}
+          //   onClick={handleClick}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
@@ -55,12 +56,56 @@ const Experience = ({ children }: Props) => {
           </div>
         </div>
         {isClicked && (
-          <div className="">
-            <p className="text-xl">
-              Developpeur Full Stack{" "}
-              <span className="text-greenlight font-semibold">@ Sharewood</span>
-            </p>
-            <p className="text-gray text-sm">Nov 2022 - Nov 2023</p>
+          <div className="pt-5 w-2/5">
+            <div>
+              <p className="text-xl">
+                Développeur Full Stack{" "}
+                <a href="https://sharewood.team/" target="_blank">
+                  <span className="text-greenlight font-semibold">
+                    @Sharewood
+                  </span>
+                </a>
+              </p>
+              <p className="text-gray text-sm">Nov 2022 - Nov 2023</p>
+            </div>
+            <div className="pt-5 flex gap-5 ">
+              <BsArrowRightShort className="text-greenlight w-[30px] h-[16px] " />
+              <p className="text-graydark">
+                Chez <span className="text-greenlight ">Sharewood</span>, j'ai
+                été impliqué dans une variété de projets, allant de petites
+                missions de suivi et de maintenance sur des sites{" "}
+                <span className="text-greenlight font-semibold">
+                  CMS WordPress
+                </span>
+                , où la gestion des mises à jour et du contenu était cruciale.
+              </p>
+            </div>
+            <div className="pt-5 flex gap-5 ">
+              <BsArrowRightShort className="text-greenlight w-[30px] h-[16px] " />
+              <p className="text-graydark">
+                En parallèle, j'ai développé des applications avec{" "}
+                <span className="text-greenlight font-semibold">Vue.js</span> et{" "}
+                <span className="text-greenlight font-semibold">Laravel</span>{" "}
+                en back-end pour différents clients, assurant une expérience
+                utilisateur optimale et des fonctionnalités avancées.
+              </p>
+            </div>
+            <div className="pt-5 flex gap-5 ">
+              <BsArrowRightShort className="text-greenlight w-[30px] h-[16px] " />
+              <p className="text-graydark">
+                J'ai également pris en charge l'hébergement et le déploiement
+                d'une application web, veillant à sa mise en ligne fluide et
+                efficace.
+              </p>
+            </div>
+            <div className="pt-5 flex gap-5 ">
+              <BsArrowRightShort className="text-greenlight w-[30px] h-[16px] " />
+              <p className="text-graydark">
+                Tout au long de ces missions, ma priorité a été de produire un
+                code propre, bien organisé et optimisé pour garantir des
+                performances optimales et une maintenance simplifiée.
+              </p>
+            </div>
           </div>
         )}
       </div>
