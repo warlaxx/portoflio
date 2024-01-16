982084d5ff66ec37c2f670c2f8006d1a43c34f5c
Abdallah Abdallaoui-maane <abdallah42.2001@icloud.com>
2024-01-16 14:25:34 +0100

    push

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import Link from "next/link";
 
 const Projets = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="w-full" id="project">
       <div className="flex items-center w-full gap-6 pt-10">
@@ -14,31 +17,43 @@ const Projets = () => {
         </div>
         <div className="w-2/5 border-t-[0.5px] border-y-graydark"></div>
       </div>
-      <div className="pt-10 flex justify-center">
-        <Image
-          src="/image/screenshot_DCGG.png"
-          alt="Description de l'image"
-          width={300} // Largeur souhaitée de l'image
-          height={100} // Hauteur souhaitée de l'image
-          className="rounded-xl"
-        />
+      <div className="pt-10 flex flex-col md:flex-row items-center ">
+        <div className="pt-10 flex flex-col md:flex-row items-center justify-center relative">
+          <Link href={"https://dc.gg/"} target="_blank">
+            <div
+              className={`absolute top-10 left-0 right-0 bottom-0 bg-greenlight transition-opacity duration-300 z-20 rounded-md ${
+                isHovered ? "opacity-0" : "opacity-40"
+              }`}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            ></div>
 
-        <div className="justify-center flex flex-col items-center text-right">
-          <p className="text-2xl text-greenlight font-bold">Project DC.GG</p>
-          <p className="text-gray px-20 py-10 bg-blue-600">
+            <Image
+              src="/image/screenshot_DCGG.png"
+              alt="Description de l'image"
+              width={600} // Largeur souhaitée de l'image
+              height={100} // Hauteur souhaitée de l'image
+              className="rounded-md mb-4 md:mb-0  relative" // Ajoutez des marges selon vos préférences
+            />
+          </Link>
+        </div>
+
+        {/* Contenu texte */}
+        <div className="md:text-end block relative right-12 w-2/5 justify-self-end z-50">
+          <p className="text-xs text-greenlight font-mono">Featured Project</p>
+          <p className="text-2xl text-gray font-bold font-mono ">
+            Project DC.GG
+          </p>
+          <p className="text-graydark  bg-[#112240] mb-4 rounded-md p-[25px] ">
             Explore dc.gg, the perfect site for Discord server administrators.
             Create personalized invitation links and track real-time statistics,
             including the number of clicks and other vital data. Simplify your
             server management and enhance your online presence with dc.gg!
           </p>
-          <p className="text-graydark">Next.js Express.js TypeScript Prisma</p>
-          <a
-            href="https://dc.gg/"
-            target="_blank"
-            className="justify-self-end flex text-right"
-          >
-            <FaExternalLinkAlt className="text-greenlight" />
-          </a>
+
+          <p className="text-graydark font-mono text-sm justify-between">
+            Next.js Express.js TypeScript Prisma
+          </p>
         </div>
       </div>
     </div>
