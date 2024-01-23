2f37450aa1b1aca2e9a9026e9a7520b5cd387bb7
Abdallah Abdallaoui-maane <abdallah42.2001@icloud.com>
2024-01-23 12:29:51 +0100

    add link britanny chiang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 import Image from "next/image";
 import Navbar from "./components/Navbar";
@@ -55,6 +56,16 @@ export default function Home() {
                 <Projets />
 
                 <Contact />
+              </div>
+
+              <div>
+                <Link
+                  href="https://v4.brittanychiang.com"
+                  target="_blank"
+                  className="text-graydark font-mono text-[11px] justify-center flex pb-5 "
+                >
+                  Inspired by Brittany Chiang's portfolio
+                </Link>
               </div>
             </div>
           </div>
